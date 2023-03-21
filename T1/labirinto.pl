0d4ca0789e4c27eae0f3a4ac2bcd4579cef5ffa3
tomassantunes <tomasbantunes@gmail.com>
2023-03-21 23:17:06 +0000

    continuação trabalho, c, d feitas

--- T1/labirinto.pl
+++ T1/labirinto.pl
@@ -15,7 +15,7 @@ bloqueado((3,3)).
 bloqueado((3,4)).
 bloqueado((3,5)).
 
-percorridos((3,1)).
+percorridos((6,1)).
 
 nao_percorridos(A) :- 
     \+ percorridos(A),
@@ -24,33 +24,26 @@ nao_percorridos(A) :-
 op((X, Y), sobe, (X, B), 1) :-
     tamanho(T), Y < T, B is Y + 1, \+ bloqueado((X,B)), nao_percorridos((X,B)).
 
+op((X, Y), esquerda, (A, Y), 1) :-
+    X > 0, A is X - 1, \+ bloqueado((A,Y)), nao_percorridos((A,Y)).
+
 op((X, Y), desce, (X, B), 1) :-
     Y > 0, B is Y - 1, \+ bloqueado((X,B)), nao_percorridos((X,B)).
 
 op((X, Y), direita, (A, Y), 1) :-
     tamanho(T), X < T, A is X + 1, \+ bloqueado((A,Y)), nao_percorridos((A,Y)).
 
-op((X, Y), esquerda, (A, Y), 1) :-
-    X > 0, A is X - 1, \+ bloqueado((A,Y)), nao_percorridos((A,Y)).
-
 % b)
 :- dynamic(max_memoria/1).
 :- dynamic(nos/1).
 
-max_memoria(0).
-nos(0).
+% max_memoria(0).
+% nos(0).
 
 inc:- retract(nos(N)), N1 is N+1, assertz(nos(N1)).
 
 actmax(N):- max_memoria(N1), N1 >= N,!.
 actmax(N):- retract(max_memoria(_N1)), assertz(max_memoria(N)).
-
-pesquisa_largura([no(E,Pai,Op,C,P)|_],no(E,Pai,Op,C,P)):- estado_final(E), inc.
-pesquisa_largura([E|R],Sol):- 
-    inc, expande(E,Lseg),
-    insere_fim(Lseg,R,Resto),
-    length(Resto,N), actmax(N),
-    pesquisa_largura(Resto,Sol).
 
 pesquisa_profundidade([no(E,Pai,Op,C,P)|_],no(E,Pai,Op,C,P)) :- estado_final(E), inc.
 pesquisa_profundidade([E|R],Sol):- 
@@ -63,26 +56,19 @@ expande(no(E,Pai,Op,C,P),L):-
             findall(no(En,no(E,Pai,Op,C,P),Opn,Cnn,P1),
             (op(E,Opn,En,Cn),P1 is P+1, Cnn is Cn+C), L).
 
-escreve_seq_solucao(no(E,Pai,Op,Custo,Prof)):- write(custo(Custo)),nl,
-                                          write(profundidade(Prof)),nl,
-                                          escreve_seq_accoes(no(E,Pai,Op,_,_)).
+escreve_seq_solucao(no(E,Pai,Op,Custo,Prof)):- 
+    write(custo(Custo)),nl,
+    write(profundidade(Prof)),nl,
+    escreve_seq_accoes(no(E,Pai,Op,_,_)).
 
 escreve_seq_accoes([]).
 escreve_seq_accoes(no(E,Pai,Op,_,_)):- escreve_seq_accoes(Pai), write(e(Op,E)),nl.
 
-pesquisa_l :-
-    estado_inicial(S0),
-    pesquisa_largura([no(S0,[],[],0,0)], Sol), nl,
-    max_memoria(M), write(lista(M)), nl,
-    escreve_seq_solucao(Sol), nl,
-    write('Estados visitados: '),
-    retract(nos(X)),
-    write(X), nl,
-    write('Máximo em memória: '),
-    retract(max_memoria(Y)),
-    write(Y).
+insere_inicio(X, Y, L) :- append(X, Y, L).
+insere_fim(X, Y, L) :- append(Y, X, L).
 
-pesquisa_p:-
+pesquisa_p :-
+    assertz(max_memoria(0)), assertz(nos(0)),
     estado_inicial(S0),
 	pesquisa_profundidade([no(S0,[],[],0,0)], Sol), nl,
     max_memoria(M), write(lista(M)), nl,
@@ -94,12 +80,64 @@ pesquisa_p:-
     retract(max_memoria(Y)),
     write(Y).
 
-insere_fim([], L, L).
-insere_fim(L, [], L).
-insere_fim(R, [A|S], [A|L]) :- insere_fim(R, S, L).
-insere_inicio(A,B,C) :- append(A, B, C).
+% e)
+h1((X, Y), R):- 
+    estado_final(PF),
+    distancia((X, Y), PF, R).
 
-max(X, X, X).
-max(X, Y, Z) :-
-    X > Y, Z is X;
-    Y > X, Z is Y.
+h2(P1, D) :- 
+    estado_final(PF),  
+    euclidean_distance(P1, PF, D).
+
+% distância de Manhattan
+distancia((X, Y), (W, Z), D):-
+    X1 is X-W,
+    abs(X1, AX),
+    Y1 is Y-Z,
+    abs(Y1, AY),
+    D is AX+AY.
+
+euclidean_distance((X1, Y1), (X2, Y2), R) :- R is sqrt((X2-X1)^2 + (Y2-Y1)^2).
+
+abs(X, X):- X > 0.
+abs(X, R):- R is -X.
+
+pesquisa_a([no(E,Pai,Op,C,HC,P)|_],no(E,Pai,Op,C,HC,P)) :- estado_final(E), inc.
+pesquisa_a([E|R],Sol) :- 
+    inc, expande_a(E,Lseg),
+    insere_ordenado(Lseg,R,Resto),
+    length(Resto,N), actmax(N),
+    pesquisa_a(Resto,Sol).
+
+expande_a(no(E,Pai,Op,C,HC,P),L) :- 
+    findall(no(En,no(E,Pai,Op,C,HC,P),Opn,Cnn,HCnn,P1),
+	(op(E,Opn,En,Cn), P1 is P+1, Cnn is Cn+C, h2(En,H), HCnn is Cnn+H), L).
+
+insere_ordenado([],L,L).
+insere_ordenado([A|T], L, LF):- 
+	ins_ord(A,L,L1), insere_ordenado(T, L1, LF).
+
+ins_ord(E, [], [E]).
+ins_ord(no(E,Pai,Op,C,CH,P), [no(E1,Pai1,Op1,C1,CH1,P1)|T], [no(E,Pai,Op,C,CH,P),no(E1,Pai1,Op1,C1,CH1,P1)|T]) :- CH =< CH1.
+ins_ord(no(E,Pai,Op,C,CH,P), [no(E1,Pai1,Op1,C1,CH1,P1)|T], [no(E1,Pai1,Op1,C1,CH1,P1)|T1]) :- ins_ord(no(E,Pai,Op,C,CH,P), T, T1).	
+
+escreve_seq_solucao_a(no(E,Pai,Op,Custo,_HC,Prof)) :-
+    write(custo(Custo)),nl,
+    write(profundidade(Prof)),nl,
+    escreve_seq_accoes_a(no(E,Pai,Op,_,_,_)).
+
+escreve_seq_accoes_a([]).
+escreve_seq_accoes_a(no(E,Pai,Op,_,_,_)) :- escreve_seq_accoes_a(Pai), write(e(Op,E)),nl.
+
+pesquisa_a :-
+    assertz(max_memoria(0)), assertz(nos(0)),
+    estado_inicial(S0),
+	pesquisa_a([no(S0,[],[],0,0,0)], Sol), nl,
+    max_memoria(M), write(lista(M)), nl,
+    escreve_seq_solucao_a(Sol), nl,
+    write('Estados visitados: '),
+    retract(nos(X)),
+    write(X), nl,
+    write('Máximo em memória: '),
+    retract(max_memoria(Y)),
+    write(Y).
--- T1/resolucao.md
+++ T1/resolucao.md
@@ -7,70 +7,73 @@ estado_final((0, 4), _).
 verificar_limite(_, []).
 verificar_limite(A, [B|L]) :- \+ A = B, verificar_limite(A, L).
 
-op(((X, Y), LISTA), sobe, ((A, B), LISTA), 1) :-
-    tamanho(T), Y < T, A is X, B is Y + 1, verificar_limite((A, B), LISTA).
+op((X, Y), sobe, (X, B), 1) :-
+    tamanho(T), Y < T, B is Y + 1, \+ bloqueado((X,B)), nao_percorridos((X,B)).
 
-op(((X, Y), LISTA), desce, ((A, B), LISTA), 1) :-
-    Y > 0, A is X, B is Y - 1, verificar_limite((A, B), LISTA).
+op((X, Y), esquerda, (A, Y), 1) :-
+    X > 0, A is X - 1, \+ bloqueado((A,Y)), nao_percorridos((A,Y)).
 
-op(((X, Y), LISTA), direita, ((A, B), LISTA), 1) :-
-    tamanho(T), Y < T, A is X + 1, B is Y, verificar_limite((A, B), LISTA).
+op((X, Y), desce, (X, B), 1) :-
+    Y > 0, B is Y - 1, \+ bloqueado((X,B)), nao_percorridos((X,B)).
 
-op(((X, Y), LISTA), esquerda, ((A, B), LISTA), 1) :-
-    Y > 0, A is X - 1, B is Y, verificar_limite((A, B), LISTA).
+op((X, Y), direita, (A, Y), 1) :-
+    tamanho(T), X < T, A is X + 1, \+ bloqueado((A,Y)), nao_percorridos((A,Y)).
+
 ```
 
 ## b)
 ```prolog
-:- dynamic(maxNL/1).
+:- dynamic(max_memoria/1).
 :- dynamic(nos/1).
 
-maxNL(0).
+max_memoria(0).
 nos(0).
 
-limpa:-retractall(maxNL(A)),
-    retractall(nos(A)), asserta(maxNL(0)),
-    asserta(nos(0)).
+inc:- retract(nos(N)), N1 is N+1, assertz(nos(N1)).
 
-inc:- retract(nos(N)), N1 is N+1, asserta(nos(N1)).
+actmax(N):- max_memoria(N1), N1 >= N,!.
+actmax(N):- retract(max_memoria(_N1)), assertz(max_memoria(N)).
 
-actmax(N):- maxNL(N1), N1 >= N,!.
-actmax(N):- retract(maxNL(_N1)), asserta(maxNL(N)).
-
-pesquisa(Alg):-
-    limpa,
-    estado_inicial(S0),
-    pesquisa(Alg,[no(S0,[],[],0,0)],Solucao),
-    nos(Ns),maxNL(NL),
-    write(nos(visitados(Ns),lista(NL))),
-    limpa,
-    escreve_seq_solucao(Solucao).
-
-pesquisa(largura,Ln,Sol):- pesquisa_largura(Ln,Sol).
-
-pesquisa_largura([no(E,Pai,Op,C,P)|_],no(E,Pai,Op,C,P)):- estado_final(E), inc.
-pesquisa_largura([E|R],Sol):- inc,
-                              expande(E,Lseg),
-                              insere_fim(Lseg,R,Resto),
-                              length(Resto,N), actmax(N),
-                              pesquisa_largura(Resto,Sol).
+pesquisa_profundidade([no(E,Pai,Op,C,P)|_],no(E,Pai,Op,C,P)) :- estado_final(E), inc.
+pesquisa_profundidade([E|R],Sol):- 
+	inc, expande(E,Lseg),
+    insere_inicio(Lseg,R,LFinal),
+    length(LFinal, L), actmax(L),
+    pesquisa_profundidade(LFinal,Sol).
 
 expande(no(E,Pai,Op,C,P),L):-
             findall(no(En,no(E,Pai,Op,C,P),Opn,Cnn,P1),
             (op(E,Opn,En,Cn),P1 is P+1, Cnn is Cn+C), L).
 
-insere_fim([],L,L).
-insere_fim(L,[],L).
-insere_fim(R,[A|S],[A|L]):- insere_fim(R,S,L).
-
-escreve_seq_solucao(no(E,Pai,Op,Custo,Prof)):- write(custo(Custo)),nl,
-                                          write(profundidade(Prof)),nl,
-                                          escreve_seq_accoes(no(E,Pai,Op,_,_)).
+escreve_seq_solucao(no(E,Pai,Op,Custo,Prof)):- 
+    write(custo(Custo)),nl,
+    write(profundidade(Prof)),nl,
+    escreve_seq_accoes(no(E,Pai,Op,_,_)).
 
 escreve_seq_accoes([]).
 escreve_seq_accoes(no(E,Pai,Op,_,_)):- escreve_seq_accoes(Pai), write(e(Op,E)),nl.
+
+insere_inicio(X, Y, L) :- append(X, Y, L).
+insere_fim(X, Y, L) :- append(Y, X, L).
+
+pesquisa_p:-
+    estado_inicial(S0),
+	pesquisa_profundidade([no(S0,[],[],0,0)], Sol), nl,
+    max_memoria(M), write(lista(M)), nl,
+    escreve_seq_solucao(Sol), nl,
+    write('Estados visitados: '),
+    retract(nos(X)),
+    write(X), nl,
+    write('Máximo em memória: '),
+    retract(max_memoria(Y)),
+    write(Y).
 ```
 
 ## c)
-+ i. 39.
-+ ii. 6.
++ i. 15.
++ ii. 11.
+
+## d)
+As duas heurísticas que propomos são: Distância de Manhattan e a Distância Euclidiana.
+
+## e)
