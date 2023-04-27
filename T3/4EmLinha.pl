3943e2aab26224cb18e6eb06170e6edb704f1271
Berna.rv <bernardo.rv.dev@gmail.com>
2023-04-27 23:51:26 +0100

    começo

---
+++ T3/4EmLinha.pl
@@ -0,0 +1,14 @@
+estado_inicial(e([coluna(1, [v,v,v,v,v,v,v]),
+                  coluna(2, [v,v,v,v,v,v,v]),
+                  coluna(3, [v,v,v,v,v,v,v]),
+                  coluna(4, [v,v,v,v,v,v,v])
+                  coluna(5, [v,v,v,v,v,v,v])
+                  coluna(6, [v,v,v,v,v,v,v])], b)).
+
+
+terminal(e([coluna(1, [v,v,v,v,v,v,v]),
+            coluna(2, [v,v,v,v,v,v,v]),
+            coluna(3, [v,v,v,v,v,v,v]),
+            coluna(4, [v,v,v,v,v,v,v])
+            coluna(5, [v,v,v,v,v,v,v])
+            coluna(6, [v,v,v,v,v,v,v])], _))
