220fbc63cad06686616eec5d1f59e511226f394b
Berna.rv <bernardo.rv.dev@gmail.com>
2023-05-16 11:20:43 +0100

    4 trabalho

---
+++ T4/blocos.pl
@@ -0,0 +1,16 @@
+% Estados
+estado_inicial([vizinhos(0,1), vizinhos(1,0), vizinhos(1,2), vizinhos(2,1), vizinhos(2,3), vizinhos(3,2),
+                mao_livre, robot_na_casa(0), bloco_na_casa(a,0), bloco_na_casa(b,1), bloco_na_casa(c,2), casa_livre(3)]).
+
+estado_final([vizinhos(0,1), vizinhos(1,0), vizinhos(1,2), vizinhos(2,1), vizinhos(2,3), vizinhos(3,2),
+              robot_na_casa(0), casa_livre(0), bloco_na_casa(c,1), bloco_na_casa(a,2), bloco_na_casa(b, 3)]).
+
+
+% Acções
+% accao(a1,Precond,AddList,DeleteList).
+
+accao(andar(C1,C2), [robot_na_casa(C1), vizinhos(C1,C2)], [robot_na_casa(C2)], [robot_na_casa(C1)]):- member(C1, [0,1,2,3]), member(C2, [0,1,2,3]), C1\=C2.
+
+accao(apanhar(B,C), [mao_livre, bloco_na_casa(B,C), robot_na_casa(C)], [na_mao(B), casa_livre(C)], [mao_livre, bloco_na_casa(B,C)]):- member(B, [a,b,c]), member(C, [0,1,2,3]).
+
+accao(larga(B,C), [na_mao(B), robot_na_casa(C), casa_livre(C)], [bloco_na_casa(B,C), mao_livre], [na_mao(B), casa_livre(C)]):- member(B, [a,b,c]), member(C, [0,1,2,3]).
