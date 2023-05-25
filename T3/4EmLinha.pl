a7475909bb48025b919e5f127218397c2e3fcef4
Berna-RV <bernardo.rv.dev@gmail.com>
2023-05-25 15:02:00 +0100

    farto

--- T3/4EmLinha.pl
+++ T3/4EmLinha.pl
@@ -1,14 +1,49 @@
-estado_inicial(e([coluna(1, [v,v,v,v,v,v,v]),
-                  coluna(2, [v,v,v,v,v,v,v]),
-                  coluna(3, [v,v,v,v,v,v,v]),
-                  coluna(4, [v,v,v,v,v,v,v])
-                  coluna(5, [v,v,v,v,v,v,v])
-                  coluna(6, [v,v,v,v,v,v,v])], b)).
+% Each column is a term col(Num,Free,TP,TN,Ps), where:
+% Num: column number
+% Free: yes/no, whether a piece can be placed "on top" (= at the end)
+% TP: Colour of topmost piece
+% TN: max. number of consecutive topmost pieces of same colour
+% Ps: Pieces in this column
+empty(-).
 
+estado_inicial(e([col(1,yes, empty, 0, Empty),
+                  col(2,yes, empty, 0, Empty),
+                  col(3,yes, empty, 0, Empty), 
+                  col(4,yes, empty, 0, Empty),
+                  col(5,yes, empty, 0, Empty),
+                  col(6,yes, empty, 0, Empty)])).
 
-terminal(e([coluna(1, [v,v,v,v,v,v,v]),
-            coluna(2, [v,v,v,v,v,v,v]),
-            coluna(3, [v,v,v,v,v,v,v]),
-            coluna(4, [v,v,v,v,v,v,v])
-            coluna(5, [v,v,v,v,v,v,v])
-            coluna(6, [v,v,v,v,v,v,v])], _))
+terminal(Board, Player):- (four_in_a_row(Board, Player) ; diagonal(Board, Player)).
+
+four_in_a_row([Col1,Col2,Col3,Col4|Cs], Player):-
+    (   four_in_a_row(Col1, Col2, Col3, Col4, Player)
+    ;   four_in_a_row([Col2,Col3,Col4|Cs], Player)
+    ).
+
+four_in_a_row([C1|Cs1], [C2|Cs2], [C3|Cs3], [C4|Cs4], P) :-
+    empty(E),
+    Firsts = [C1,C2,C3,C4],
+    maplist(dif(E), Firsts),
+    (   maplist(=(P), Firsts)
+    ;   four_in_a_row(Cs1, Cs2, Cs3, Cs4, P)
+    ).
+
+diagonal(Board, Player) :-
+    Board = [_,_,_,_|_],
+    (   diagonal_down(Board, Player)
+    ;   diagonal_up(Board, Player)
+    ;   Board = [_|Rest],
+        diagonal(Rest, Player)
+    ).
+
+diagonal_down([Col1,Col2,Col3,Col4|_], Player) :-
+        Col2 = [_|Rot2],
+        Col3 = [_,_|Rot3],
+        Col4 = [_,_,_|Rot4],
+        four_in_a_row(Col1, Rot2, Rot3, Rot4, Player).
+
+diagonal_up([Col1,Col2,Col3,Col4|_], Player) :-
+        Col1 = [_,_,_|Rot1],
+        Col2 = [_,_|Rot2],
+        Col3 = [_|Rot3],
+        four_in_a_row(Rot1, Rot2, Rot3, Col4, Player).
--- T4/blocos.pl
+++ T4/blocos.pl
@@ -32,14 +32,20 @@
 
 accao(agarra(B,C), [mao_livre, robot_na_casa(C), na_casa(B,C)], [na_mao(B), casa_vazia(C)], [mao_livre, na_casa(B,C)]) :- member(C,[0,1,2,3]), member(B,[a,b,c]).
 accao(larga(B,C), [na_mao(B), casa_vazia(C), robot_na_casa(C)], [na_casa(B,C), mao_livre], [na_mao(B), casa_vazia(C)]) :- member(C,[0,1,2,3]), member(B,[a,b,c]).
-accao(move(C1,C2), [vizinho(C1,C2), robot_na_casa(C1)], [robot_na_casa(C2)], [robot_na_casa(C1)]) :- member(C1,[0,1,2,3]), member(C2,[0,1,2,3]), C1\=C2.
+accao(move(A,B), [vizinho(A,B), robot_na_casa(A)], [robot_na_casa(B)], [robot_na_casa(A)]) :- member(A,[0,1,2,3]), member(B,[0,1,2,3]), A\=B.
 
 % estado_inicial([vizinho(0,1), vizinho(1,0), vizinho(1,2), vizinho(2,1), vizinho(2,3), vizinho(3,2),
 %     robot_na_casa(0), mao_livre, na_casa(a,0), na_casa(b, 1), na_casa(c,2), casa_vazia(3)]).
-estado_inicial([vizinho(0,1), vizinho(1,0), vizinho(1,2), vizinho(2,1), vizinho(2,3), vizinho(3,2),
-    robot_na_casa(2), na_mao(b), na_casa(a,0), casa_vazia(1), na_casa(c,2), casa_vazia(3)]).
+%estado_inicial([vizinho(0,1), vizinho(1,0), vizinho(1,2), vizinho(2,1), vizinho(2,3), vizinho(3,2),
+%    robot_na_casa(2), na_mao(b), na_casa(a,0), casa_vazia(1), na_casa(c,2), casa_vazia(3)]).
 
 % estado_final([vizinho(0,1), vizinho(1,0), vizinho(1,2), vizinho(2,1), vizinho(2,3), vizinho(3,2),
 %     robot_na_casa(0), mao_livre, casa_vazia(0), na_casa(c,1), na_casa(a, 2), na_casa(b,3)]).
-estado_final([vizinho(0,1), vizinho(1,0), vizinho(1,2), vizinho(2,1), vizinho(2,3), vizinho(3,2),
-    robot_na_casa(3), mao_livre, na_casa(a,1), casa_vazia(0), na_casa(c, 2), na_casa(b,3)]).
+%estado_final([vizinho(0,1), vizinho(1,0), vizinho(1,2), vizinho(2,1), vizinho(2,3), vizinho(3,2),
+%    robot_na_casa(3), mao_livre, na_casa(a,1), casa_vazia(0), na_casa(c, 2), na_casa(b,3)]).
+
+estado_inicial([a_direita(1,0), a_direita(2,1), a_direita(3,2), a_esquerda(0,1), a_esquerda(1,2), a_esquerda(3,2),
+              mao_livre, robot_na_casa(0), bloco_na_casa(a,1), bloco_na_casa(b,2), bloco_na_casa(c,3), casa_livre(0)]).
+              
+estado_final([a_direita(1,0), a_direita(2,1), a_direita(3,2), a_esquerda(0,1), a_esquerda(1,2), a_esquerda(3,2),
+              mao_livre, robot_na_casa(1), bloco_na_casa(a,0), bloco_na_casa(b,2), bloco_na_casa(c,3), casa_livre(1)]).
