3f02dfa5a843617410a9d6bd23375c75c9f9dc6f
Berna-RV <bernardo.rv.dev@gmail.com>
2023-05-29 15:08:24 +0100

    ainda não

--- T3/minimax.pl
+++ T3/minimax.pl
@@ -1,4 +1,4 @@
-g(Jogo):- [Jogo], estado_inicial(Ei), minimax_decidir(Ei,Op), write(Op),nl.
+g(Jogo):- [Jogo], estado_inicial(Ei), minimax_decidir(Ei,Op), write(Ei), nl, write(Op), nl.
 
 % decide qual é a melhor jogada num estado do jogo
 % minimax_decidir(Estado, MelhorJogada)
@@ -9,9 +9,10 @@ minimax_decidir(Ei,terminou):- terminal(Ei).
 %Para cada estado sucessor de Ei calcula o valor minimax do estado
 %Opf é o operador (jogada) que tem maior valor
 
-minimax_decidir(Ei,Opf):- 
-findall(Es-Op, op1(Ei,Op,Es),L),
-findall(Vc-Op,(member(E-Op,L), minimax_valor(E,Vc,1)),L1), escolhe_max(L1,Opf).
+minimax_decidir(Ei,Opf):- findall(Es-Op, op1(Ei,Op,Es),L),
+                          findall(Vc-Op,(member(E-Op,L), 
+                          minimax_valor(E,Vc,1)),L1), 
+                          escolhe_max(L1,Opf).
 
 % se um estado é terminal o valor é dado pela função de utilidade
 minimax_valor(Ei,Val,P):- terminal(Ei), valor(Ei,Val,P).
@@ -20,9 +21,8 @@ minimax_valor(Ei,Val,P):- terminal(Ei), valor(Ei,Val,P).
 % -se a profundidade é par, o maior valor dos sucessores de Ei
 % -se aprofundidade é impar o menor valor dos sucessores de Ei
 minimax_valor(Ei,Val,P):- findall(Es,op1(Ei,_,Es),L),
-P1 is P+1,
-findall(Val1,(member(E,L),minimax_valor(E,Val1,P1)),V), seleciona_valor(V,P,Val).
-
+                          P1 is P+1,
+                          findall(Val1,(member(E,L),minimax_valor(E,Val1,P1)),V), seleciona_valor(V,P,Val).
 
 % Se a profundidade (P) é par, retorna em Val o maximo de V
 seleciona_valor(V,P,Val):- X is P mod 2, X=0,!, maximo(V,Val).
@@ -37,13 +37,11 @@ maximo([],A,A).
 maximo([A|R],X,Val):- A < X,!, maximo(R,X,Val).
 maximo([A|R],_,Val):- maximo(R,A,Val).
 
-
 escolhe_max([A|R],Val):- escolhe_max(R,A,Val).
 
 escolhe_max([],_-Op,Op).
 escolhe_max([A-_|R],X-Op,Val):- A < X,!, escolhe_max(R,X-Op,Val).
 escolhe_max([A|R],_,Val):- escolhe_max(R,A,Val).
-
 
 minimo([A|R],Val):- minimo(R,A,Val).
 
